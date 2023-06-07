f49ed2dd26ba390581f6197e6f6ef6375b8bc853
Fardeem Munir <munirfardeem@gmail.com>
2023-06-07 17:32:21 -0500

    Make it nicer

--- app.py
+++ app.py
@@ -6,15 +6,20 @@ from text_processing import get_articles
 
 def process_image(image):
     ocr_result = get_ocr_result(image)
+    yield "Extracting articles...", ocr_result
     results = get_articles(ocr_result)
-    return f"{results}\n\n\nOCR Result:\n{ocr_result}"
+    yield results, ocr_result
 
 
 demo = gr.Interface(
     fn=process_image,
     inputs=gr.Image(type="filepath", label="Upload an image"),
-    outputs="text",
+    outputs=[
+        gr.Textbox(label="Extracted Articles"),
+        gr.Textbox(label="Image OCR"),
+    ],
     title="Extracting Articles from Newspaper Images",
 )
 
+demo.queue()
 demo.launch()
